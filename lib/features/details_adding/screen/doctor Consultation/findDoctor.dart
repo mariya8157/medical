5f3028d4a51cc477b202c3e20b0c7dd31142e9f2
rafee <rafeeqmariya@gmail.com>
2024-03-28 15:59:58 +0530

    initial comit

--- lib/features/details_adding/screen/doctor Consultation/findDoctor.dart
+++ lib/features/details_adding/screen/doctor Consultation/findDoctor.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:medical/colour.dart';
 import 'package:medical/icons.dart';
@@ -51,142 +53,145 @@ class _FindDoctorState extends State<FindDoctor> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //     centerTitle: true,
-      //     backgroundColor: Colors.white24,
-      //     // resizeToAvoidBottomInset: false,
-      //     elevation: 0,
-      //     leading: InkWell(
-      //       onTap: () {
-      //         Navigator.pop(context);
-      //       },
-      //       child: SizedBox(
-      //         height: width * 0.05,
-      //         width: width * 0.8,
-      //         child: Padding(
-      //           padding: EdgeInsets.only(left: width*0.023),
-      //           child: Padding(
-      //             padding:  EdgeInsets.all(width*0.007),
-      //             child:
-      //             SvgPicture.asset(
-      //               iconConst.back,),
-      //           ),
-      //         ),
-      //       ),
-      //     ),
-      //     title:  Text(
-      //       "Articles",
-      //       style: TextStyle(
-      //           color: Colors.black,
-      //           fontWeight: FontWeight.w700,
-      //           fontSize: width*0.063
-      //       ),
-      //     ),
-      //     actions: [Row(
-      //         children: [
-      //           // SvgPicture.asset(iconConst.columnDot),
-      //           SizedBox(width: width*0.05,)
-      //         ],)
-      //         ],
-      //     ),
-
-      body:SingleChildScrollView(
-        child:Column(
-          children: [
-            TextFormField(
-              controller:searchController ,
-              keyboardType: TextInputType.name,
-              textInputAction: TextInputAction.search,
-              decoration: InputDecoration(
-                  prefixIcon:Icon(Icons.search),
-                  labelText: "Search any Product..",
-                  labelStyle: TextStyle(
-                    fontSize: width*0.05,
-                    fontWeight: FontWeight.w400,
-                  ),
-                  hintText: "please enter search product",
-                  hintStyle: TextStyle(
-                    fontSize: width*0.05,
-                    fontWeight: FontWeight.w400,
-                  ),
-                  suffixIcon: Icon(Icons.keyboard_voice_outlined),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                      ),
-                      borderRadius: BorderRadius.circular(
-                          width*0.03)
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(width*0.03),
-                      borderSide: BorderSide(
-                      )
-                  )
+      appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.white24,
+          // resizeToAvoidBottomInset: false,
+          elevation: 0,
+          leading: InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: SizedBox(
+              height: width * 0.05,
+              width: width * 0.8,
+              child: Padding(
+                padding: EdgeInsets.only(left: width*0.023),
+                child: Padding(
+                  padding:  EdgeInsets.all(width*0.007),
+                  child:
+                  SvgPicture.asset(
+                    ImageIcons.back,),
+                ),
               ),
             ),
-            // Container(
-            //
-            //   child: Column(
-            //     children: [
-            //       Text("Category",
-            //         style: TextStyle(
-            //             fontSize: width*0.05,
-            //             fontWeight: FontWeight.w800,
-            //             color:Colour.thirdcolour
-            //         ),),
-            //       Expanded(
-            //         child: GridView.builder(
-            //           itemCount:category.length,
-            //           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            //             childAspectRatio: 1,
-            //             crossAxisSpacing: width*0.05,
-            //             mainAxisSpacing: width*0.05,
-            //             crossAxisCount: 3,
-            //           ),
-            //           physics: BouncingScrollPhysics(),
-            //           shrinkWrap: true,
-            //           scrollDirection: Axis.horizontal,
-            //           itemBuilder: (context, index) {
-            //             return
-            //               Container(
-            //                 width: width*1,
-            //                 height: width*0.5,
-            //                 decoration: BoxDecoration(
-            //                   color: Colors.white,
-            //                   borderRadius: BorderRadius.circular(width*0.03),
-            //                   border: Border.all(
-            //                       color: Colors.black,
-            //                       width: width*0.008
-            //                   ),
-            //                 ),
-            //                 child: Column(mainAxisAlignment: MainAxisAlignment.spaceAround,
-            //                   children: [
-            //
-            //                     Container(
-            //                       width: width*0.2,
-            //                       height: width*0.2,
-            //                       child: SvgPicture.asset(category[index]["image"]),
-            //                     ),
-            //
-            //                     Text(category[index]["text"],)],),
-            //               );
-            //
-            //           },
-            //
-            //
-            //         ),
-            //       ),
-            //
-            //
-            //
-            //     ],
-            //   ),
-            // ),
+          ),
+          title:  Text(
+            "Find Doctors",
+            style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w700,
+                fontSize: width*0.063
+            ),
+          ),
+          actions: [Row(
+              children: [
+                SvgPicture.asset(ImageIcons.columnDot),
+                SizedBox(width: width*0.05,)
+              ],)
+              ],
+          ),
+
+      body:Column(
+        children: [
+          TextFormField(
+            controller:searchController ,
+            keyboardType: TextInputType.name,
+            textInputAction: TextInputAction.search,
+            decoration: InputDecoration(
+                prefixIcon:Icon(Icons.search),
+                labelText: "Search any Product..",
+                labelStyle: TextStyle(
+                  fontSize: width*0.05,
+                  fontWeight: FontWeight.w400,
+                ),
+                hintText: "please enter search product",
+                hintStyle: TextStyle(
+                  fontSize: width*0.05,
+                  fontWeight: FontWeight.w400,
+                ),
+                suffixIcon: Icon(Icons.keyboard_voice_outlined),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                    ),
+                    borderRadius: BorderRadius.circular(
+                        width*0.03)
+                ),
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(width*0.03),
+                    borderSide: BorderSide(
+                    )
+                )
+            ),
+          ),
+          SizedBox(height: width*0.03,),
+          Row(
+            children: [
+              Text("Category",
+                style: TextStyle(
+                    fontSize: width*0.05,
+                    fontWeight: FontWeight.w800,
+                    color:Colour.thirdcolour
+                ),),
+            ],
+          ),
+          SizedBox(height: width*0.03,),
+
+          Expanded(
+            child: GridView.builder(
+              itemCount:category.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                childAspectRatio: 1,
+                crossAxisSpacing: width*0.009,
+                mainAxisSpacing: width*0.009,
+                crossAxisCount: 4,
+              ),
+              // physics: BouncingScrollPhysics(),
+              // shrinkWrap: true,
+              // scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return
+                  Row(
+                    children: [
+                      Container(
+                        width: width*0.2,
+                        height: width*0.25,
+                        // decoration: BoxDecoration(
+                        //   color: Colors.white,
+                        //   borderRadius: BorderRadius.circular(width*0.03),
+                        //   border: Border.all(
+                        //       color: Colors.black,
+                        //       width: width*0.008
+                        //   ),
+                        // ),
+                        child: Column(mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+
+                            Container(
+                              width: width*0.05,
+                              height: width*0.05,
+                              child: SvgPicture.asset(category[index]["image"],)
+                            ),
+
+                            Text(category[index]["text"],)
+                          ],
+                        ),
+                      ),
+                    ],
+                  );
+
+              },
 
 
-          ],
-        )
+            ),
+          ),
 
-          ,
+
+
+
+
+
+        ],
       ),
 
     );
--- lib/icons.dart
+++ lib/icons.dart
@@ -2,7 +2,7 @@ class ImageIcons{
   static const catogory1="assets/icons/Doctor.svg";
   static const catogory2="assets/icons/Lungs.svg";
   static const catogory3="assets/icons/Dentist.svg";
-  static const catogory4="assets/icons/Dentist.svg";
+  static const catogory4="assets/icons/Category.svg";
   static const catogory5="assets/icons/Covid.svg";
   static const catogory6="assets/icons/Syringe.svg";
   static const catogory7="assets/icons/health.svg";
