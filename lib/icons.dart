bb4c2cdb350d778afb342fd30df75895b98ee429
rafee <rafeeqmariya@gmail.com>
2024-03-28 13:50:39 +0530

    initial comit

--- lib/icons.dart
+++ lib/icons.dart
@@ -7,9 +7,9 @@ class ImageIcons{
   static const catogory6="assets/icons/Syringe.svg";
   static const catogory7="assets/icons/health.svg";
   static const catogory8="assets/icons/leftIcon.svg";
-  static const back = "assets/icos/back.svg";
-  static const columnDot = "assets/icos/3 dot.svg";
-  static const search = "assets/icos/Search.svg";
+  static const back = "assets/icons/back.svg";
+  static const columnDot = "assets/icons/3 dot.svg";
+  static const search = "assets/icons/Search.svg";
 
 
 
