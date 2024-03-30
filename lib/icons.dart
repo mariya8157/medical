eb6e03f2dc6ae6a68a12be662527e0899e6bfbd5
DELL <pcpkousar@gmail.com>
2024-03-30 11:00:11 +0530

    new changes1

--- lib/icons.dart
+++ lib/icons.dart
@@ -7,35 +7,42 @@ class ImageIcons{
   static const catogory6="assets/icons/Syringe.svg";
   static const catogory7="assets/icons/health.svg";
   static const catogory8="assets/icons/leftIcon.svg";
-  static const catogory9="assets/icons/Psychiatrist.svg";
+  static const catogory9="assets/icons/leftIcon.svg";
   static const back = "assets/icons/back.svg";
   static const columnDot = "assets/icons/3 dot.svg";
   static const search = "assets/icons/Search.svg";
+  static const save = "assets/icons/save.svg";
   static const email = "assets/icons/email.svg";
   static const lock = "assets/icons/lock.svg";
   static const eyeOff = "assets/icons/eyeoff.svg";
   static const google = "assets/icons/google.svg";
   static const apple = "assets/icons/apple.svg";
   static const facebook = "assets/icons/facebook.svg";
-  static const save = "assets/icons/save.svg";
   static const star = "assets/icons/Star.svg";
   static const location = "assets/icons/Location.svg";
 
 
-
   ///png
-  static const covid1 = "assets/image/covid_19_1.png";
-  static const medics="assets/image/medics.png";
-  static const covid2 = "assets/image/covid_19_2.png";
-  static const covid3 = "assets/image/covid_19_3.png";
-  static const covid4 = "assets/image/covid_19_4.png";
-  static const covid5 = "assets/image/covid_19_5.png";
-  static const covid6 = "assets/image/covid_19_6.png";
-  static const covid7 = "assets/image/covid_19_7.png";
+  static const covid1 = "assets/images/covid_19_1.png";
+  static const medics="assets/images/medics.png";
+  static const covid2 = "assets/images/covid_19_2.png";
+  static const covid3 = "assets/images/covid_19_3.png";
+  static const covid4 = "assets/images/covid_19_4.png";
+  static const covid5 = "assets/images/covid_19_5.png";
+  static const covid6 = "assets/images/covid_19_6.png";
+  static const covid7 = "assets/images/covid_19_7.png";
   static const drmarcus = "assets/images/drimage.png";
   static const drmaria = "assets/images/drmaria.png";
   static const drstevi = "assets/images/drStevi.png";
   static const drluke = "assets/images/drLuke.png";
+  static const tablet = "assets/images/tablet.png";
+  static const panadol = "assets/images/panadol.png";
+  static const herbal = "assets/images/herbal.png";
+  static const konidin = "assets/images/konidin.png";
+  static const calvit = "assets/images/calvit.png";
+
+
+
 
 
 
