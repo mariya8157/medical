225ead7cc9f820806956595a0ad6679647a1e0f5
rafee <rafeeqmariya@gmail.com>
2024-03-30 11:36:08 +0530

    initial comit

---
+++ lib/features/details_adding/screen/home/topdoctor.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TopDoctorPage extends StatefulWidget {
+  const TopDoctorPage({super.key});
+
+  @override
+  State<TopDoctorPage> createState() => _TopDoctorPageState();
+}
+
+class _TopDoctorPageState extends State<TopDoctorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
