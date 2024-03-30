a29c02935e8d945a66caca2f4cee8da3d8b6dc78
rafee <rafeeqmariya@gmail.com>
2024-03-30 11:32:13 +0530

    initial comit

---
+++ lib/features/details_adding/screen/doctor Consultation/messagehistory.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MessageHistoryPage extends StatefulWidget {
+  const MessageHistoryPage({super.key});
+
+  @override
+  State<MessageHistoryPage> createState() => _MessageHistoryPageState();
+}
+
+class _MessageHistoryPageState extends State<MessageHistoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
