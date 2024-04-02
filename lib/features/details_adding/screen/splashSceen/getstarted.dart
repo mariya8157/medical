ae091623d2f3f1d2c5d147401e09b982779ff3d6
Hp <rashamuneersulthan@gmail.com>
2024-04-02 12:47:11 +0530

    initial comit

--- lib/features/details_adding/screen/splashSceen/getstarted.dart
+++ lib/features/details_adding/screen/splashSceen/getstarted.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:medical/colour.dart';
 
+import '../../../../icons.dart';
 import '../../../../main.dart';
 
 class GetStartedPage extends StatefulWidget {
@@ -15,12 +17,70 @@ class _GetStartedPageState extends State<GetStartedPage> {
   Widget build(BuildContext context) {
     return  Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
-            height:width*0.35 ,
-            width: width*0.35,
-            color: Colour.primarycolour,
-          )
+          Center(
+            child: Container(
+              height: width*0.35,
+              width: width*0.35,
+              child: SvgPicture.asset(ImageIcons.medics1),
+          ),),SizedBox(height: width*0.05,),
+          Text("Let’s get started!",
+            style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: width*0.06
+            ),
+          ),SizedBox(height: width*0.04,),
+          Text("Login to enjoy the features we’ve\n    provided, and stay healthy!",
+            style: TextStyle(
+                color: Colour.color5,
+                fontSize: width*0.04
+            ),),
+          SizedBox(height: width*0.05,),
+          InkWell(
+            onTap: () {
+
+            },
+            child: Container(
+              height: width*0.14,
+              width: width*0.75,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(width*0.08),
+                  color:Colour.primarycolour
+              ),
+              child: Center(
+                child: Text("Login",
+                  style: TextStyle(
+                      color: Colour.secondarycolour,
+                      fontSize: width*0.05
+                  ),),
+              ),
+            ),
+          ),
+          SizedBox(height: width*0.04,),
+          InkWell(
+            onTap: () {
+
+            },
+            child: Container(
+              height: width*0.14,
+              width: width*0.75,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(width*0.08),
+                  color: Colour.secondarycolour,
+                  border: Border.all(
+                      color: Colour.primarycolour
+                  )
+              ),
+              child: Center(
+                child: Text("Sign Up",
+                  style: TextStyle(
+                      color: Colour.primarycolour,
+                      fontSize: width*0.05
+                  ),),
+              ),
+            ),
+          ),
         ],
       ),
     );
