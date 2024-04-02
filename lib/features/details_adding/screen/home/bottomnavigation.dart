0b6918c0967d597cc32fdc397aefcf9b3c895fdc
rafee <rafeeqmariya@gmail.com>
2024-04-02 13:19:57 +0530

    initial comit

---
+++ lib/features/details_adding/screen/home/bottomnavigation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigationPage extends StatefulWidget {
+  const BottomNavigationPage({super.key});
+
+  @override
+  State<BottomNavigationPage> createState() => _HomeState();
+}
+
+class _HomeState extends State<BottomNavigationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
