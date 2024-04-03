92a0a0602cfbf47282b9504b8431cf8b5ca7831f
Hp <rashamuneersulthan@gmail.com>
2024-04-03 10:53:06 +0530

    initial comit

--- lib/features/details_adding/screen/home/topdoctor.dart
+++ lib/features/details_adding/screen/home/topdoctor.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../icons.dart';
+import '../../../../main.dart';
 
 class TopDoctorPage extends StatefulWidget {
   const TopDoctorPage({super.key});
@@ -8,8 +12,82 @@ class TopDoctorPage extends StatefulWidget {
 }
 
 class _TopDoctorPageState extends State<TopDoctorPage> {
+  List drlist=[
+    {
+      "image":ImageIcons.drmarcus,
+      "name":"Dr. Marcus",
+      "spl":"Chardiologist",
+      "star":"4,7",
+      "distene":"800m away",
+
+    },
+    {
+      "image":ImageIcons.drmaria,
+      "name":"Dr. Maria",
+      "spl":"Gynecologist",
+      "star":"4,7",
+      "distene":"500m away",
+
+    },
+    {
+      "image":ImageIcons.drstevi,
+      "name":"Dr. Stevi",
+      "spl":"dermatologist",
+      "star":"4,8",
+      "distene":"900m away",
+
+    }
+    ,{
+      "image":ImageIcons.drluke,
+      "name":"Dr. Luke",
+      "spl":"General medicine",
+      "star":"4,7",
+      "distene":"800m away",
+
+    }
+
+
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.white24,
+          // resizeToAvoidBottomInset: false,
+          elevation: 0,
+          leading: InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: SizedBox(
+              height: width * 0.05,
+              width: width * 0.8,
+              child: Padding(
+                padding: EdgeInsets.only(left: width*0.023),
+                child: Padding(
+                  padding:  EdgeInsets.all(width*0.007),
+                  child:
+                  SvgPicture.asset(
+                    ImageIcons.back,),
+                ),
+              ),
+            ),
+          ),
+          title:  Text(
+            "Top Doctor",
+            style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w700,
+                fontSize: width*0.063
+            ),
+          ),
+          actions: [Row(
+            children: [
+              SvgPicture.asset(ImageIcons.columnDot),
+              SizedBox(width: width*0.05,)
+            ],),]
+      ),
+    );
   }
 }
