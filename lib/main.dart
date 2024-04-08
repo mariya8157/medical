ba6545f43e8796a247273eebf0a0561741608cbd
rafee <rafeeqmariya@gmail.com>
2024-04-08 13:19:11 +0530

    initial comit

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,6 @@ var width;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(  MyApp());
 }
