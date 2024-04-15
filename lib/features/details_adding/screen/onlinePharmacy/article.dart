0fb3a94e1f10a8c0498976b35889455aab061d63
DELL <pcpkousar@gmail.com>
2024-04-15 15:08:32 +0530

    new changes 4

--- lib/features/details_adding/screen/onlinePharmacy/article.dart
+++ lib/features/details_adding/screen/onlinePharmacy/article.dart
@@ -69,7 +69,7 @@ class _articlesState extends State<articles> {
     },
     {
       "image":ImageIcons.covid6,
-      "text1":"Beauty Tips For Face: 10 Dos and \n Naturally Beautiful Skin",
+      "text1":"Beauty Tips For Face: 10 Dos  \n Naturally Beautiful Skin",
       "icon":ImageIcons.save,
       "text2":"Jun 10, 2021 . ",
       "text3":"7min read",
