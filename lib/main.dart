3a707f4e2d2d9909fa5a1ed651238dfc49cff6df
DELL <pcpkousar@gmail.com>
2024-04-23 11:43:18 +0530

    new changes 4

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
             theme: ThemeData(
               textTheme: GoogleFonts.interTextTheme()
             ),
-            home:SplashScreen()
+            home:ProfilePage()
     ),);
   }
 }
