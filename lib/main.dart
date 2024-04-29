986e6bb71b0ebaa280416db58396bb180069fc93
rafee <rafeeqmariya@gmail.com>
2024-04-29 13:33:01 +0530

    initial comit

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
             theme: ThemeData(
               textTheme: GoogleFonts.interTextTheme()
             ),
-            home:MyCartPage(d: [],)
+            home:SplashScreen()
     ),);
   }
 }
