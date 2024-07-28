6dac2dd696b89a6b1e5c206a210bf2cae1464e05
Hp <rashamuneersulthan@gmail.com>
2024-07-28 18:26:54 +0530

    initial comit

--- lib/models/hospital_model.dart
+++ lib/models/hospital_model.dart
@@ -1,33 +1,39 @@
 class HospitalModel{
   String image;
   String text;
+  String id;
 
   HospitalModel({
     required this.image,
-    required this.text
+    required this.text,
+    required this.id
 });
 
   Map<String,dynamic> toMap(){
     return{
       'image':this.image,
-      'text':this.text
+      'text':this.text,
+      'id':this.id
     };
   }
   factory HospitalModel.fromMap(Map<String,dynamic>map){
     return HospitalModel(
         image: map['image']??"",
         text: map['text']??"",
+      id: map['id']??""
     );
   }
   HospitalModel copyWith({
     String? image,
     String? text,
+    String? id
 
   })
   {
     return HospitalModel(
         image:  image ?? this.image,
-        text: text ?? this.text
+        text: text ?? this.text,
+      id: id ?? this.id
     );
   }
 }
