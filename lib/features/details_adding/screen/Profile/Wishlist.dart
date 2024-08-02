2bad101ffac34463bb90b53a919f90e32c8f47de
HP 840 G4 <basilakk7@gmail.com>
2024-08-02 17:31:26 +0530

    initial commit

--- lib/features/details_adding/screen/Profile/Wishlist.dart
+++ lib/features/details_adding/screen/Profile/Wishlist.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:pinput/pinput.dart';
 
 import '../../../../core/constants/colour.dart';
 import '../../../../core/constants/icons.dart';
@@ -53,144 +54,144 @@ class _MySavedState extends ConsumerState<MySaved> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                  child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-                    stream: FirebaseFirestore.instance.collection("users").snapshots(),
-                    builder: (context, snapshot){
-                      if(!snapshot.hasData){
-                        return Center(child:Text("Loading..."));
-                      }
-                      else{
-                        Text("No Products added to My Saved");
-                      }
-                      var data =currentModel!.wish;
-                      return Container(
-                        height: height*1,
-                        width: width*1,
-                        child: GridView.builder(
-                          itemCount: data.length,
-                            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                                childAspectRatio: 1.5,
-                                crossAxisSpacing:width*0.05,
-                                mainAxisSpacing: height*0.05,
-                                crossAxisCount: 2),
-                          itemBuilder: (context, index) {
-                            Column(
-                              children: [
-                                Container(
-                                  height: width * 0.6,
-                                  width: width * 0.45,
-                                  margin: EdgeInsets.only(left: width * 0.055),
-                                  decoration: BoxDecoration(
-                                      border: Border.all(
-                                          color: Colour.gray.withOpacity(0.2)),
-                                      borderRadius:
-                                      BorderRadius.circular(width * 0.05)),
-                                  child: Padding(
-                                    padding: EdgeInsets.all(width * 0.017),
-                                    child: Column(
-                                      children: [
-                                        SizedBox(height: width * 0.01,),
-                                        Container(
-                                          height: width * 0.3,
-                                          width: width * 0.3,
-                                          child: Image(image:NetworkImage(data[index]['image'].toString()),fit: BoxFit.fill,
-                                            width: width * 0.27,
-                                          ),
-                                        ),
-                                        Row(
+              StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                stream: FirebaseFirestore.instance.collection("users").snapshots(),
+                builder: (context, snapshot){
+                  if(!snapshot.hasData){
+                    return Center(child:Text("Loading..."));
+                  }
+                  else{
+                    Text("No Products added to My Saved");
+                  }
+                  var data =currentModel!.wish;
+                  return ListView.separated(
+                    shrinkWrap: true,
+                    physics: BouncingScrollPhysics(),
+                    itemCount:data.length,
+                    itemBuilder: (context, index) {
+                      print('--------------------------------------------------');
+                      print(currentModel!.wish);
+                      print('--------------------------------------------------');
+                      Column(
+                        children: [
+                          Container(
+                            height: width * 0.6,
+                            width: width * 0.45,
+                            margin: EdgeInsets.only(left: width * 0.055),
+                            decoration: BoxDecoration(
+
+                                border: Border.all(
+                                    color: Colour.gray.withOpacity(0.2)),
+                                borderRadius:
+                                BorderRadius.circular(width * 0.05)),
+                            child: Padding(
+                              padding: EdgeInsets.all(width * 0.017),
+                              child: Column(
+                                children: [
+                                  SizedBox(height: width * 0.01,),
+                                  Container(
+                                    height: width * 0.3,
+                                    width: width * 0.3,
+                                    child: Image(image:NetworkImage(data[index]['image'].toString()),fit: BoxFit.fill,
+                                      width: width * 0.27,
+                                    ),
+                                  ),
+                                  Row(
+                                    children: [
+                                      Container(
+                                        height: width * 0.15,
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                                           children: [
                                             Container(
-                                              height: width * 0.15,
-                                              child: Column(
-                                                crossAxisAlignment: CrossAxisAlignment.start,
-                                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                              child: Row(
                                                 children: [
-                                                  Container(
-                                                    child: Row(
-                                                      children: [
-                                                        Text(
-                                                          data[index]['name'].toString(),
-                                                          style: TextStyle(
-                                                              fontWeight: FontWeight.w700,
-                                                              fontSize: width * 0.035),
-                                                        ),
-                                                      ],
-                                                    ),
-                                                  ),
                                                   Text(
-                                                    data[index]['ml'].toString(),
+                                                    data[index]['name'].toString(),
                                                     style: TextStyle(
-                                                        fontSize: width * 0.035,
-                                                        color: Colour.gray,
-                                                        fontWeight: FontWeight.w500),
+                                                        fontWeight: FontWeight.w700,
+                                                        fontSize: width * 0.035),
                                                   ),
                                                 ],
                                               ),
                                             ),
+                                            Text(
+                                              data[index]['ml'].toString(),
+                                              style: TextStyle(
+                                                  fontSize: width * 0.035,
+                                                  color: Colour.gray,
+                                                  fontWeight: FontWeight.w500),
+                                            ),
                                           ],
                                         ),
-                                        Container(
-                                          height: width * 0.10,
-                                          width: width * 0.45,
-                                          child: Row(
-                                            mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                            children: [
-                                              Text(
-                                                "\$${data[index]["rate"].toString()}",
-                                                style: TextStyle(
-                                                    fontWeight: FontWeight.w700,
-                                                    fontSize: width * 0.035),
-                                              ),
-                                              // InkWell(
-                                              //     onTap: () {
-                                              //       setState(() {
-                                              //
-                                              //         if (favorite.contains(index)) {
-                                              //           favorite.remove(index);
-                                              //         } else {
-                                              //           favorite.add(index);
-                                              //           MedicineModel wishlist= MedicineModel(
-                                              //               name: data[selectindex].name,
-                                              //               image: data[selectindex].image.toString(),
-                                              //               ml: data[selectindex].ml.toString(),
-                                              //               rate: data[selectindex].rate,
-                                              //               off: data[selectindex].off,
-                                              //               id: data[selectindex].id.toString(),
-                                              //               des: data[selectindex].des.toString(),
-                                              //               qty: data[selectindex].qty,
-                                              //               userId: userId!);
-                                              //           ref.read(AddingControllerProvider).updatewish(currentModel!,wishlist);
-                                              //         }
-                                              //       });
-                                              //     },
-                                              //     child: !favorite.contains(index)
-                                              //         ? SvgPicture.asset(
-                                              //       ImageIcons.c_add,
-                                              //       width: width * 0.05,
-                                              //     )
-                                              //         : SvgPicture.asset(
-                                              //       ImageIcons.heart,
-                                              //       width: width * 0.06,
-                                              //     ))
-                                              SvgPicture.asset(ImageIcons.heart)
-                                            ],
-                                          ),
-                                        )
+                                      ),
+                                    ],
+                                  ),
+                                  Container(
+                                    height: width * 0.10,
+                                    width: width * 0.45,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          "\$${data[index]["rate"].toString()}",
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.w700,
+                                              fontSize: width * 0.035),
+                                        ),
+                                        // InkWell(
+                                        //     onTap: () {
+                                        //       setState(() {
+                                        //
+                                        //         if (favorite.contains(index)) {
+                                        //           favorite.remove(index);
+                                        //         } else {
+                                        //           favorite.add(index);
+                                        //           MedicineModel wishlist= MedicineModel(
+                                        //               name: data[selectindex].name,
+                                        //               image: data[selectindex].image.toString(),
+                                        //               ml: data[selectindex].ml.toString(),
+                                        //               rate: data[selectindex].rate,
+                                        //               off: data[selectindex].off,
+                                        //               id: data[selectindex].id.toString(),
+                                        //               des: data[selectindex].des.toString(),
+                                        //               qty: data[selectindex].qty,
+                                        //               userId: userId!);
+                                        //           ref.read(AddingControllerProvider).updatewish(currentModel!,wishlist);
+                                        //         }
+                                        //       });
+                                        //     },
+                                        //     child: !favorite.contains(index)
+                                        //         ? SvgPicture.asset(
+                                        //       ImageIcons.c_add,
+                                        //       width: width * 0.05,
+                                        //     )
+                                        //         : SvgPicture.asset(
+                                        //       ImageIcons.heart,
+                                        //       width: width * 0.06,
+                                        //     ))
+                                        SvgPicture.asset(ImageIcons.heart)
                                       ],
                                     ),
-                                  ),
-                                ),
-                                // SizedBox(height: width*0.03,),
-                              ],
-                            );
-                          },
-                        ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                          // SizedBox(height: width*0.03,),
+                        ],
                       );
                     },
-                  )
+                    separatorBuilder: (context, index) {
+                      return SizedBox(
+                        height: width * 0.05,
+                      );
+                    },
+                  );
 
+                },
               ),
             ],
           ),
